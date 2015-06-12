a2da343f7aa5434fcbc9c076a3f6ee9570617836
Luke Valentine <lucifargundam@users.noreply.github.com>
2015-06-12 01:43:30 -0700

    Update nexus.c
    a little something for now.

--- nexus.c
+++ nexus.c
@@ -2,5 +2,12 @@
 
 main ()
 {
-printf ("Hello &user and welcome to XXXXXX!\n");
+char an, pat[] = {};
+
+printf ("Hello &user and welcome to XXXXXX!\n\n\n");
+printf ("Please choose which of the following patterns for your results:\n");
+printf ("Traditional, Golden, 12 Kings, Trinity, Random");
+scanf ("%s\n\n", pat);
+printf ("Would you like inverse results available? (Y/N)";
+scanf ("%c", an);
 }
